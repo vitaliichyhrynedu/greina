bead53b21af17de279c8972fb22658d16d13a709
Vitalii Chyhryn <chyhryn.vitalii+edu@gmail.com>
2025-12-10 08:14:50 +0200

    fs: implement the node structure

---
+++ src/kernel/fs/node.rs
@@ -0,0 +1,31 @@
+use zerocopy::{FromBytes, Immutable, IntoBytes};
+
+use crate::hardware::storage::block::BLOCK_SIZE;
+
+/// [Node] size.
+pub const NODE_SIZE: usize = size_of::<Node>();
+
+/// How many nodes fit in a [Block].
+pub const NODES_PER_BLOCK: usize = BLOCK_SIZE / NODE_SIZE;
+
+/// How many extents a [Node] can have.
+const MAX_EXTENTS: usize = 15;
+
+/// Represents a file system object.
+#[repr(C)]
+#[derive(Default, Clone, Copy)]
+#[derive(FromBytes, IntoBytes, Immutable)]
+pub struct Node {
+    pub size: u64,
+    pub link_count: u64,
+    pub extents: [Extent; MAX_EXTENTS],
+}
+
+/// Represents a contigous span of blocks.
+#[repr(C)]
+#[derive(Default, Clone, Copy)]
+#[derive(FromBytes, IntoBytes, Immutable)]
+pub struct Extent {
+    pub start_block: usize,
+    pub length: usize,
+}
